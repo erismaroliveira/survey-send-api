8623b94791d5a244545af10d130997967299fa31
erismar <erismarpro@hotmail.com>
2021-10-11 01:36:23 -0300

    users controller

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
+
+class UsersController {
+
+    async create(req: Request, res: Response) {
+        const { name, email } = req.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        });
+
+        try {
+            await schema.validate(req.body, { abortEarly: false });
+        } catch (error) {
+            throw new AppError(error);
+        }
+        
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists){
+            throw new AppError("User already exists!");
+        }
+
+        const user = usersRepository.create({
+            name, 
+            email
+        });
+
+        await usersRepository.save(user);
+
+        return res.status(201).json(user);
+    }
+}
+
+export { UsersController };
