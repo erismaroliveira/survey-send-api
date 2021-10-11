f3ff6fea528383eea9a5a7ef84db006716ae83eb
erismar <erismarpro@hotmail.com>
2021-10-11 01:18:16 -0300

    routes application

---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { AnswerController } from "./controllers/AnswerController";
+import { NpsController } from "./controllers/NpsController";
+import { SendMailController } from "./controllers/SendMailController";
+import { SurveysController } from "./controllers/SurveysController";
+import { UsersController } from "./controllers/UsersController";
+
+const router = Router();
+
+const usersController = new UsersController();
+const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
+
+router.post("/users", usersController.create);
+
+router.get("/surveys", surveysController.show);
+router.post("/surveys", surveysController.create);
+
+router.post("/sendMail", sendMailController.execute);
+router.get("/answers/:value", answerController.execute);
+router.get("/nps/:survey_id", npsController.execute);
+
+export { router };
