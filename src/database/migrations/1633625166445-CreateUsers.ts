5edf862054308daade4bcc7e803a7ff2100a5265
erismar <erismarpro@hotmail.com>
2021-10-11 01:41:59 -0300

    migration

---
+++ src/database/migrations/1633625166445-CreateUsers.ts
@@ -0,0 +1,37 @@
+import {Column, MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1633625166445 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
